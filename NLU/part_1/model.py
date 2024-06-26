f7c68603f70052923ed305663933684f9ea9c8df
ettore <ettoreitaut4.1@gmail.com>
2024-06-26 17:28:33 +0200

    DEV: added bidirectional "support" (it's a flag yay)

--- NLU/part_1/model.py
+++ NLU/part_1/model.py
@@ -11,23 +11,28 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class ModelIAS(nn.Module):
 
-    def __init__(
-        self, model_config, vocab_len, name, pad_index=0
-    ):
+    def __init__(self, model_config, vocab_len, name, pad_index=0):
         super().__init__()
         # hid_size = Hidden size
         # out_slot = number of slots (output size for slot filling)
         # out_int = number of intents (output size for intent class)
         # emb_size = word embedding size
         self.name = name
-        self.embedding = nn.Embedding(vocab_len, model_config["emb_size"], padding_idx=pad_index)
-
+        self.embedding = nn.Embedding(
+            vocab_len, model_config["emb_size"], padding_idx=pad_index
+        )
+        
+        # dropout: float = 0,
         self.utt_encoder = nn.LSTM(
-            model_config["emb_size"], model_config["hid_size"], model_config["n_layers"], bidirectional=False, batch_first=True
+            input_size=model_config["emb_size"],
+            hidden_size=model_config["hid_size"],
+            num_layers=model_config["n_layers"],
+            bidirectional=model_config["bidirectional"],
+            batch_first=True,
         )
         self.slot_out = nn.Linear(model_config["hid_size"], model_config["out_slot"])
         self.intent_out = nn.Linear(model_config["hid_size"], model_config["out_int"])
-        
+
         # Dropout layer How/Where do we apply it?
         self.dropout = nn.Dropout(0.1)
 
@@ -45,7 +50,7 @@ class ModelIAS(nn.Module):
 
         # Unpack the sequence
         utt_encoded, input_sizes = pad_packed_sequence(packed_output, batch_first=True)
-        
+
         # Get the last hidden state
         last_hidden = last_hidden[-1, :, :]
 
@@ -60,6 +65,6 @@ class ModelIAS(nn.Module):
 
         # Slot size: batch_size, seq_len, classes
         slots = slots.permute(0, 2, 1)  # We need this for computing the loss
-        
+
         # Slot size: batch_size, classes, seq_len
         return slots, intent
